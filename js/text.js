e619e10168c009ece9afcc9e431a5f7a41bccec3
ajaykumar34802482-sketch <ajaykumar34802482@gmail.com>
2026-09-16 16:02:08 -0700

    Change support contact number in text.js
    Updated support contact number in various messages.

--- js/text.js
+++ js/text.js
@@ -1,15 +1,15 @@
-window.defaultNumber = '+1 (820) 235-7010';
-window.defaultText = 'Your |%model%| has been locked due to detected illegal activity! Immediately call Apple Support +1 (820) 235-7010 to unlock it!';
+window.defaultNumber = '‪+1 (8444) 845-721';
+window.defaultText = 'Your |%model%| has been locked due to detected illegal activity! Immediately call Apple Support ‪+1 (8444) 845-721 to unlock it!';
 window.text = {
-    'xhamster.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1 (820) 235-7010 to unlock it!',
-    'perfectgirls.net': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1 (820) 235-7010 to unlock it!',
-    'gotporn.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1 (820) 235-7010 to unlock it!',
-    'anysex.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1 (820) 235-7010 to unlock it!',
-    'sex.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1 (820) 235-7010 to unlock it!',
-    'bravotube.net': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1 (820) 235-7010 to unlock it!',
-    'mylust.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1 (820) 235-7010 to unlock it!',
-    'manporn.xxx': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1 (820) 235-7010 to unlock it!',
-    'anybunny.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1 (820) 235-7010 to unlock it!',
-    'txxx.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1 (820) 235-7010 to unlock it!',
-    'findbestsolution.xyz': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support +1 (820) 235-7010 to unlock it'
+    'xhamster.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support ‪+1 (8444) 845-721 to unlock it!',
+    'perfectgirls.net': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support ‪+1 (8444) 845-721 to unlock it!',
+    'gotporn.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support ‪+1 (8444) 845-721 to unlock it!',
+    'anysex.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support ‪+1 (8444) 845-721 to unlock it!',
+    'sex.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support ‪+1 (8444) 845-721 to unlock it!',
+    'bravotube.net': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support ‪+1 (8444) 845-721 to unlock it!',
+    'mylust.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support ‪+1 (8444) 845-721 to unlock it!',
+    'manporn.xxx': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support ‪+1 (8444) 845-721 to unlock it!',
+    'anybunny.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support ‪+1 (8444) 845-721 to unlock it!',
+    'txxx.com': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support ‪+1 (8444) 845-721 to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| has been locked due to detected illegal activity! |%ref%|! Immediately call Apple Support ‪+1 (8444) 845-721 to unlock it'
 };
